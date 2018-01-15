932e6564cd3fb4ad2a16de8e2d0842870d777827
phonghm <huynhminhphong93@gmail.com>
2018-01-16 00:25:39 +0700

    add new sign up activity

--- FirstLoginApp/app/src/main/java/com/example/phong_pc/firstloginapp/LoginActivity.java
+++ FirstLoginApp/app/src/main/java/com/example/phong_pc/firstloginapp/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.phong_pc.firstloginapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnSignUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        final Intent intent = new Intent(this, SignUpActivity.class);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }

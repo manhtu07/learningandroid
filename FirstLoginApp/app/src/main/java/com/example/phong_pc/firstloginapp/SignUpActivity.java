f22abaa55b78b31b871b5fc9b21eec7afc8c9463
phonghm <huynhminhphong93@gmail.com>
2018-01-16 11:11:24 +0700

    add new app

---
+++ FirstLoginApp/app/src/main/java/com/example/phong_pc/firstloginapp/SignUpActivity.java
@@ -0,0 +1,62 @@
+package com.example.phong_pc.firstloginapp;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText edBirthDay;
+    TextView txtSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edBirthDay = (EditText) findViewById(R.id.edBirthday);
+
+        edBirthDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePicker();
+            }
+        });
+
+        final Intent intent = new Intent(this,LoginActivity.class);
+
+        txtSignUp = (TextView) findViewById(R.id.txtSignUp);
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void showDatePicker(){
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int date = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(i,i1,i2);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                edBirthDay.setText(simpleDateFormat.format(calendar.getTime()));
+            }
+        }, year,month,date);
+        datePickerDialog.show();
+    }
+}

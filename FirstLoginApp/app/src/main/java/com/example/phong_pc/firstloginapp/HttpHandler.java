0b73aa1077a0533dcace0495c3be0768e849ca40
DESKTOP-P8K1RHD\Linh <quangvulinh@gmail.com>
2018-01-16 14:43:33 +0700

    Function login.

---
+++ FirstLoginApp/app/src/main/java/com/example/phong_pc/firstloginapp/HttpHandler.java
@@ -0,0 +1,121 @@
+package com.example.phong_pc.firstloginapp;
+
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by Linh on 1/16/2018.
+ */
+
+public class HttpHandler {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+    private static final String TAG = HttpHandler.class.getSimpleName();
+
+    public HttpHandler() {
+    }
+
+    public String makeServiceGet(String reqUrl) {
+        String response = null;
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            // read the response
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            response = convertStreamToString(in);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "MalformedURLException: " + e.getMessage());
+        } catch (ProtocolException e) {
+            Log.e(TAG, "ProtocolException: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+        return response;
+    }
+
+    public  String makeServicePostOrPut(String url,String jsonString,String actionName) {
+
+        StringBuffer response = new StringBuffer();
+        try {
+            // Send post request
+            String method="POST";
+            if(actionName==""){
+                method="PUT";
+                url+=actionName;
+            }
+
+            URL obj = new URL(url);
+
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            //add request header
+            con.setRequestProperty("Content-length", String.valueOf(jsonString.length()));
+            con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            //con.setRequestProperty("User-Agent", "Opera/9.26 (X11; Linux i686; U; en)");
+
+
+            con.setRequestMethod(method);
+            con.setDoOutput(true);
+            con.setDoInput(true);
+
+            // send data into server
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(jsonString);
+            wr.flush();
+            wr.close();
+
+            // get status code server return
+            int responseCode = con.getResponseCode();
+            if (responseCode == 200) {
+
+                // get content server return
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+
+                // read content server return
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return response.toString();
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+}
--- FirstLoginApp/app/src/main/java/com/example/phong_pc/firstloginapp/LoginActivity.java
+++ FirstLoginApp/app/src/main/java/com/example/phong_pc/firstloginapp/LoginActivity.java
@@ -1,23 +1,43 @@
 package com.example.phong_pc.firstloginapp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button btnSignUp;
+    Button btnSignUp,btnLogin;
+    EditText edUsername,edPassword;
+    private ProgressDialog pDialog;
+    private String TAG = LoginActivity.class.getSimpleName();
+    private static String url = "http://172.27.9.23/api/User/";
+    private boolean isSuccess=false;
 
-
+    public void doChangeUi()
+    {
+        final Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         btnSignUp = (Button) findViewById(R.id.btnSignUp);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        edUsername   = (EditText)findViewById(R.id.edUsername);
+        edPassword   = (EditText)findViewById(R.id.edPassword);
+
         final Intent intent = new Intent(this, SignUpActivity.class);
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +46,76 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isSuccess=false;
+                new Authentication(getApplicationContext()).execute();
+            }
+        });
+    }
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class Authentication extends AsyncTask<Void, Void, Void> {
+        Context context;
+
+        private Authentication(Context context) {
+            this.context = context.getApplicationContext();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+            String jsonStr = "";
+            try {
+
+                jsonStr=sh.makeServiceGet(url+edUsername.getText().toString());
+
+                if (jsonStr != null) {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    String password = (String) jsonObj.get("Password");
+                    String userPass=edPassword.getText().toString();
+
+                    if(password.equalsIgnoreCase(userPass)){
+                        isSuccess=true;
+                    }
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /** Updating parsed JSON data**/
+            if(isSuccess){
+                Toast.makeText(getApplicationContext(),
+                        "Login success",
+                        Toast.LENGTH_LONG)
+                        .show();
+                context.startActivity(new Intent(context, MainActivity.class));
+
+            }
+        }
     }
 }
